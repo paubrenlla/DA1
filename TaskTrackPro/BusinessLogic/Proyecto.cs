89229f6566cdd83f969b319c9a2f86c15106777a
Mateo <mateomcelano@gmail.com>
2025-04-11 19:46:26 -0300

    refactor: test constructor con parametros

---
+++ TaskTrackPro/BusinessLogic/Proyecto.cs
@@ -0,0 +1,14 @@
+﻿namespace BusinessLogic;
+
+public class Proyecto
+{
+    public string? Nombre { get; set; }
+    public string? Descripcion { get; set; }
+    public DateTime FechaInicio { get; set; }
+    public Proyecto(string nombre, string descripcion, DateTime fechaInicio)
+    {
+        Nombre = nombre;
+        Descripcion = descripcion;
+        FechaInicio = fechaInicio;
+    }
+}
--- TaskTrackPro/BusinessLogic_Tests/ProyectoTests.cs
+++ TaskTrackPro/BusinessLogic_Tests/ProyectoTests.cs
@@ -1,4 +1,6 @@
-﻿namespace BusinessLogic_Tests;
+﻿using BusinessLogic;
+
+namespace BusinessLogic_Tests;
 
 [TestClass]
 public class ProyectoTests
@@ -19,17 +21,3 @@ public class ProyectoTests
     
 }
 
-public class Proyecto
-{
-    public string? Nombre { get; set; }
-    public string? Descripcion { get; set; }
-    public DateTime FechaInicio { get; set; }
-    public Proyecto(string nombre, string descripcion, DateTime fechaInicio)
-    {
-        Nombre = nombre;
-        Descripcion = descripcion;
-        FechaInicio = fechaInicio;
-    }
-
-
-}
