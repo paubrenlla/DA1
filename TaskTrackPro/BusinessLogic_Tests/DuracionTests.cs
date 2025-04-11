941dabe555cca60e27ba7687db3f2b3df0bd14b9
fgavello <fgavello@ipxon.com>
2025-04-11 20:48:26 -0300

    RED: validación de TipoDuracion inválido

--- TaskTrackPro/BusinessLogic_Tests/DuracionTests.cs
+++ TaskTrackPro/BusinessLogic_Tests/DuracionTests.cs
@@ -19,9 +19,15 @@ namespace BusinessLogic_Tests
             {
                 var duracion = new Duracion(-5, TipoDuracion.Horas);
             });
-
-
-
+        }
+        [TestMethod]
+        public void ConstructorConTipoInvalido_LanzaArgumentException()
+        {
+            TipoDuracion tipoInvalido = (TipoDuracion)99; // un valor que no está definido en el enum
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                var duracion = new Duracion(5, tipoInvalido);
+            });
         }
     }
 }
