1ec9fbebee23a26147b85c8a9fee0aa2fbbc5abf
Mateo <mateomcelano@gmail.com>
2025-04-11 19:40:10 -0300

    red: test constructor con parametros

---
+++ TaskTrackPro/BusinessLogic_Tests/ProyectoTests.cs
@@ -0,0 +1,20 @@
+﻿namespace BusinessLogic_Tests;
+
+[TestClass]
+public class ProyectoTests
+{
+    [TestMethod]
+    public void ProyectoConstructorConDatosCorrectos()
+    {
+        string nombre = "Proyecto A";
+        string descripcion = "Este es un proyecto para el TDD jej";
+        DateTime fechaInicio = DateTime.Today;
+
+        Proyecto proyecto = new Proyecto(nombre, descripcion, fechaInicio);
+
+        Assert.AreEqual(nombre, proyecto.Nombre);
+        Assert.AreEqual(descripcion, proyecto.Descripcion);
+        Assert.AreEqual(fechaInicio, proyecto.FechaInicio);
+    }
+    
+}
