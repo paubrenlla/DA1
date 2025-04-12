cad58c4c082daefb9b3ee62aca3dd643e336f75e
fgavello <fgavello@ipxon.com>
2025-04-11 21:01:43 -0300

    REFACTOR: validación de TipoDuracion inválido

--- TaskTrackPro/BusinessLogic/Duracion.cs
+++ TaskTrackPro/BusinessLogic/Duracion.cs
@@ -2,18 +2,34 @@ namespace BusinessLogic;
 
 public class Duracion
 {
-    private int Cantidad { get; set; }
-    private TipoDuracion Tipo { get; set; }
+    private int _cantidad;
+    private TipoDuracion _tipo;
 
     public Duracion(int cantidad, TipoDuracion tipo)
     {
-        if (cantidad <= 0)
-            throw new ArgumentException("La cantidad debe ser un número positivo mayor que cero.", nameof(cantidad));
-        
-        if (!Enum.IsDefined(typeof(TipoDuracion), tipo))
-            throw new ArgumentException("El tipo de duración no es válido.", nameof(tipo));
-        
         Cantidad = cantidad;
         Tipo = tipo;
+    }
+
+    public int Cantidad
+    {
+        get => _cantidad;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("La cantidad debe ser un número positivo mayor que cero.", nameof(value));
+            _cantidad = value;
+        }
+    }
+
+    public TipoDuracion Tipo
+    {
+        get => _tipo;
+        set
+        {
+            if (!Enum.IsDefined(typeof(TipoDuracion), value))
+                throw new ArgumentException("El tipo de duración no es válido.", nameof(value));
+            _tipo = value;
+        }
     }
 }
