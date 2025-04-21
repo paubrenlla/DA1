6d4b4dfc30cfb9398439e38a457d27a969a0bcf7
Mateo <mateomcelano@gmail.com>
2025-04-20 21:31:08 -0300

    red:Test de constructor pero ya ingresando a un usuario Admin

--- TaskTrackPro/BusinessLogic_Tests/DBTests.cs
+++ TaskTrackPro/BusinessLogic_Tests/DBTests.cs
@@ -16,4 +16,20 @@ public class DBTests
         Assert.AreEqual(0, db.ListaUsuarios.Count);
     }
     
+    [TestMethod]
+    public void ConstructorConUsarioAdmin()
+    {
+        Usuario user = new Usuario();
+        DB db = new DB(user);
+        
+        Assert.AreEqual(1, db.AdministradoresSistema.Count);
+        Assert.AreEqual(0, db.ListaProyectos.Count);
+        Assert.AreEqual(0, db.ListaRecursos.Count);
+        Assert.AreEqual(1, db.ListaUsuarios.Count);
+        Assert.AreSame(user, db.AdministradoresSistema[0]);
+        Assert.AreSame(user, db.ListaUsuarios[0]);
+    }
+    
+    
+    
 }
