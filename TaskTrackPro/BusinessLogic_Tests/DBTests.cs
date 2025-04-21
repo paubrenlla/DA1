589ba53b682ac0d11f7ee053c09b4e6befc5e919
Mateo <mateomcelano@gmail.com>
2025-04-20 21:11:54 -0300

    red:Test del contructor de DB sin agregarle nada

---
+++ TaskTrackPro/BusinessLogic_Tests/DBTests.cs
@@ -0,0 +1,18 @@
+﻿namespace BusinessLogic_Tests;
+
+[TestClass]
+public class DBTests
+{
+    [TestMethod]
+    public void ConstructorSinParametros()
+    {
+        DB db = new DB();
+        
+        Assert.AreEqual(0, db.AdministradoresSistema.Count);
+        Assert.AreEqual(0, db.ListaProyectos.Count);
+        Assert.AreEqual(0, db.ListaRecursos.Count);
+        Assert.AreEqual(0, db.ListaUsuarios.Count);
+    }
+    
+}
+
