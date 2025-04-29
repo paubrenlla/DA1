b8e02225323c7190ba153b2e03bee44cc7ee06f0
Mateo <mateomcelano@gmail.com>
2025-04-29 19:28:39 -0300

    test: recursos con ID

--- TaskTrackPro/BusinessLogic/Recurso.cs
+++ TaskTrackPro/BusinessLogic/Recurso.cs
@@ -2,12 +2,15 @@
 
 public class Recurso
 {
+    private static int _contadorId = 1;
     public string Nombre { get; set; }
     public string Tipo { get; set; }
     public string Descripcion { get; set; }
+    public int Id { get; set; }
 
     public Recurso(string nombre, string tipo, string descripcion)
     {
+        Id = _contadorId++;
         Nombre = nombre;
         Tipo = tipo;
         Descripcion = descripcion;
--- TaskTrackPro/BusinessLogic_Tests/RecursoTests.cs
+++ TaskTrackPro/BusinessLogic_Tests/RecursoTests.cs
@@ -1,10 +1,19 @@
 ﻿using BusinessLogic;
+using System.Reflection;
 
 namespace BusinessLogic_Tests;
 
 [TestClass]
 public class RecursoTests
 {
+    [TestInitialize]
+    public void Setup()
+    {
+        typeof(Recurso)
+            .GetField("_contadorId", BindingFlags.Static | BindingFlags.NonPublic)
+            ?.SetValue(null, 1);
+    }
+    
     [TestMethod]
     public void CreoRecursoConParametrosCorrectos()
     {
@@ -16,5 +25,22 @@ public class RecursoTests
         Assert.AreEqual(tipo, recurso.Tipo);
         Assert.AreEqual(descripcion, recurso.Descripcion);
     }
+    
+    [TestMethod]
+    public void RecursosConsecutivosConIDCorrecta()
+    {
+        string nombre = "Auto";
+        string tipo = "Vehiculo";
+        string descripcion = "Auto de la empresa";
+        Recurso recurso = new Recurso(nombre, tipo, descripcion);
+        Assert.AreEqual(1, recurso.Id);
+        
+        string nombre2 = "Auto2";
+        string tipo2 = "Vehiculo";
+        string descripcion2 = "Segundo auto de la empresa";
+        Recurso recurso2 = new Recurso(nombre2, tipo2, descripcion2);
+        Assert.AreEqual(2,recurso2.Id);
+    }
+    
     
 }
