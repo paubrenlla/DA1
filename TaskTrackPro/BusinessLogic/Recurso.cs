9df7dba7fa3fc01eabba5038c927728d792e5e42
paubrenlla <pau.brenlla@gmail.com>
2025-04-30 14:22:35 -0300

    feat: recurso con nuevos parametros

--- TaskTrackPro/BusinessLogic/Recurso.cs
+++ TaskTrackPro/BusinessLogic/Recurso.cs
@@ -6,19 +6,34 @@ public class Recurso
     public string Nombre { get; set; }
     public string Tipo { get; set; }
     public string Descripcion { get; set; }
-    public bool sePuedeCompartir { get; set; }
+    public bool SePuedeCompartir { get; set; }
+    public int CantidadDelRecurso { get; set; }
+    public int CantidadEnUso { get; set; }
+    public Proyecto? ProyectoAlQuePertenece { get; set; }
     public int Id { get; set; }
 
-    public Recurso(string nombre, string tipo, string descripcion, bool sePuedeCompartir)
+    public Recurso(string nombre, string tipo, string descripcion, bool sePuedeCompartir, int cantidadDelRecurso) 
+        : this(nombre, tipo, descripcion, sePuedeCompartir, cantidadDelRecurso, null) {}
+
+    public Recurso(string nombre, string tipo, string descripcion, bool sePuedeCompartir, int cantidadDelRecurso, Proyecto? proyectoAlQuePertenece) 
     {
         if (string.IsNullOrEmpty(nombre)) 
             throw new ArgumentNullException("Se debe ingresar un nombre.");
         
         if (string.IsNullOrEmpty(tipo))
             throw new ArgumentNullException("Se debe ingresar un tipo.");
+        
+        if (cantidadDelRecurso <= 0)
+            throw new ArgumentException("La cantidad del recurso debe ser mayor a 0.");
+
         Id = _contadorId++;
         Nombre = nombre;
         Tipo = tipo;
         Descripcion = descripcion;
+        SePuedeCompartir = sePuedeCompartir;
+        CantidadDelRecurso = cantidadDelRecurso;
+        CantidadEnUso = 0;
+        ProyectoAlQuePertenece = proyectoAlQuePertenece;
+
     }
 }
--- TaskTrackPro/BusinessLogic_Tests/RecursoTests.cs
+++ TaskTrackPro/BusinessLogic_Tests/RecursoTests.cs
@@ -15,30 +15,93 @@ public class RecursoTests
     }
     
     [TestMethod]
-    public void CreoRecursoConParametrosCorrectos()
+    public void CreoRecursoConParametrosCorrectosSinProyecto()
     {
         string nombre = "Auto";
         string tipo = "Vehiculo";
         string descripcion = "Auto de la empresa";
-        Recurso recurso = new Recurso(nombre, tipo, descripcion, false);
-        Assert.AreEqual(nombre,recurso.Nombre);
+        bool sePuedeCompartir = false;
+        int cantidadDelRecurso = 5; 
+        Proyecto? proyecto = null;
+
+        Recurso recurso = new Recurso(nombre, tipo, descripcion, sePuedeCompartir, cantidadDelRecurso, proyecto);
+
+        Assert.AreEqual(nombre, recurso.Nombre);
         Assert.AreEqual(tipo, recurso.Tipo);
         Assert.AreEqual(descripcion, recurso.Descripcion);
-        Assert.AreEqual(false, recurso.sePuedeCompartir);
+        Assert.AreEqual(sePuedeCompartir, recurso.SePuedeCompartir);
+        Assert.AreEqual(cantidadDelRecurso, recurso.CantidadDelRecurso);
+        Assert.AreEqual(0, recurso.CantidadEnUso); 
+        Assert.AreEqual(proyecto, recurso.ProyectoAlQuePertenece);
+    }
+    
+    [TestMethod]
+    public void CreoRecursoConParametrosCorrectosSinProyectoSobrecargado()
+    {
+        string nombre = "Auto";
+        string tipo = "Vehiculo";
+        string descripcion = "Auto de la empresa";
+        bool sePuedeCompartir = false;
+        int cantidadDelRecurso = 5;
+
+        Recurso recurso = new Recurso(nombre, tipo, descripcion, sePuedeCompartir, cantidadDelRecurso);
+
+        Assert.AreEqual(nombre, recurso.Nombre);
+        Assert.AreEqual(tipo, recurso.Tipo);
+        Assert.AreEqual(descripcion, recurso.Descripcion);
+        Assert.AreEqual(sePuedeCompartir, recurso.SePuedeCompartir);
+        Assert.AreEqual(cantidadDelRecurso, recurso.CantidadDelRecurso);
+        Assert.AreEqual(0, recurso.CantidadEnUso); 
+        Assert.AreEqual(null, recurso.ProyectoAlQuePertenece);
+    }
+    
+    [TestMethod]
+    public void CreoRecursoConParametrosCorrectosConProyecto()
+    {
+        string nombre = "Auto";
+        string tipo = "Vehiculo";
+        string descripcion = "Auto de la empresa";
+        bool sePuedeCompartir = false;
+        int cantidadDelRecurso = 5; 
+        Proyecto? proyecto = new Proyecto("Proyecto", "Descripcion", DateTime.Now);
+
+        Recurso recurso = new Recurso(nombre, tipo, descripcion, sePuedeCompartir, cantidadDelRecurso, proyecto);
+
+        Assert.AreEqual(nombre, recurso.Nombre);
+        Assert.AreEqual(tipo, recurso.Tipo);
+        Assert.AreEqual(descripcion, recurso.Descripcion);
+        Assert.AreEqual(sePuedeCompartir, recurso.SePuedeCompartir);
+        Assert.AreEqual(cantidadDelRecurso, recurso.CantidadDelRecurso);
+        Assert.AreEqual(0, recurso.CantidadEnUso); 
+        Assert.AreEqual(proyecto, recurso.ProyectoAlQuePertenece);
     }
     
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void CreoRecursoConNombreVacío()
     {
-        Recurso recurso = new Recurso("", "Vehiculo", "Auto de la empresa", false);
+        Recurso recurso = new Recurso("", "Vehiculo", "Auto de la empresa", false, 5, null);
     }
     
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void CreoRecursoSinTipo()
     {
-        Recurso recurso = new Recurso("Auto", "", "Auto de la empresa", false);
+        Recurso recurso = new Recurso("Auto", "", "Auto de la empresa", false, 5, null);
+    }
+    
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreoRecursoConCantidadDelRecursoIgualA0()
+    {
+        Recurso recurso = new Recurso("Auto", "Vechiculo", "Auto de la empresa", false, 0, null);
+    }
+    
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreoRecursoConCantidadDelRecursoMenorA0()
+    {
+        Recurso recurso = new Recurso("Auto", "Vechiculo", "Auto de la empresa", false, -8, null);
     }
     
     [TestMethod]
@@ -47,13 +110,13 @@ public class RecursoTests
         string nombre = "Auto";
         string tipo = "Vehiculo";
         string descripcion = "Auto de la empresa";
-        Recurso recurso = new Recurso(nombre, tipo, descripcion, false);
+        Recurso recurso = new Recurso(nombre, tipo, descripcion, false, 1, null);
         Assert.AreEqual(1, recurso.Id);
         
         string nombre2 = "Auto2";
         string tipo2 = "Vehiculo";
         string descripcion2 = "Segundo auto de la empresa";
-        Recurso recurso2 = new Recurso(nombre2, tipo2, descripcion2, false);
+        Recurso recurso2 = new Recurso(nombre2, tipo2, descripcion2, false, 2, null);
         Assert.AreEqual(2,recurso2.Id);
     }
    
