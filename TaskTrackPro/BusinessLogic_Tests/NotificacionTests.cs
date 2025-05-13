410dd65789f29090dab421fa7dd6832cb4e121b8
Mateo Muniz <MM242444@fi365.ort.edu.uy>
2025-05-13 08:48:10 -0300

    red: crear una notificación sin usuarios

---
+++ TaskTrackPro/BusinessLogic_Tests/NotificacionTests.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogic;
+
+namespace BusinessLogic_Tests;
+
+[TestClass]
+public class NotificacionTests
+{
+  [TestMethod]
+  public void CreoNotificacionCorrectamente()
+  {
+      string mensaje = "Esta es una notifcación de prueba";
+      Notificacion notificacion = new Notificacion(mensaje);
+      Assert.AreEqual(mensaje, notificacion.Mensaje);
+  }
+}
