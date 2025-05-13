6a8e538b57272ffbf350d35a02b8e073decc2610
Mateo Muniz <MM242444@fi365.ort.edu.uy>
2025-05-13 08:53:36 -0300

    refactor: crear una notificacion sin usuarios

--- TaskTrackPro/BusinessLogic/Notificacion.cs
+++ TaskTrackPro/BusinessLogic/Notificacion.cs
@@ -1,6 +1,15 @@
-﻿namespace BusinessLogic;
-
-public class Notificacion
+﻿public class Notificacion
 {
+    private string _mensaje;
+
+    public string Mensaje
+    {
+        get => _mensaje;
+        set => _mensaje = value;
+    }
+    public Notificacion(string mensaje)
+    {
+        Mensaje = mensaje;
+    }
     
 }
--- TaskTrackPro/BusinessLogic_Tests/NotificacionTests.cs
+++ TaskTrackPro/BusinessLogic_Tests/NotificacionTests.cs
@@ -13,5 +13,4 @@ public class NotificacionTests
       Assert.AreEqual(mensaje, notificacion.Mensaje);
   }
   
-  
 }
