f49abd5fc0855555462a35dea18dd53d9a388179
Mateo Muniz <MM242444@fi365.ort.edu.uy>
2025-05-13 09:31:56 -0300

    red: agregar usuarios a una notificacion

--- TaskTrackPro/BusinessLogic_Tests/NotificacionTests.cs
+++ TaskTrackPro/BusinessLogic_Tests/NotificacionTests.cs
@@ -13,4 +13,20 @@ public class NotificacionTests
       Assert.AreEqual(mensaje, notificacion.Mensaje);
   }
   
+  [TestMethod]
+  public void AgregeUsuariosANotificacion()
+  {
+      string mensaje = "Esta es una notifcación de prueba";
+      Notificacion notificacion = new Notificacion(mensaje);
+      
+      Usuario u = new Usuario("example@email.com", "Nombre", "Apellido", "EsValida1!", new DateTime(2000, 01, 01));
+      Usuario u2 = new Usuario("example2@email.com", "Nombre", "Apellido", "EsValida1!", new DateTime(2000, 01, 01));
+      notificacion.AgregarUsuario(u);
+      notificacion.AgregarUsuario(u2);
+
+      Assert.AreEqual(2, notificacion.UsuariosNotificados.Count);
+      Assert.IsTrue(notificacion.UsuariosNotificados.Contains(u));
+      Assert.IsTrue(notificacion.UsuariosNotificados.Contains(u2));
+  }
+
 }
