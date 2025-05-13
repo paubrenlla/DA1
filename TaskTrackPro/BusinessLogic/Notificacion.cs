c8aad4ad7f18f1d60b7439c7db597d19dfd60fa6
Mateo Muniz <MM242444@fi365.ort.edu.uy>
2025-05-13 10:01:01 -0300

    green: marcar una notificación como vista por usuarios correctamente

--- TaskTrackPro/BusinessLogic/Notificacion.cs
+++ TaskTrackPro/BusinessLogic/Notificacion.cs
@@ -4,26 +4,39 @@ public class Notificacion
 {
     private string _mensaje;
     private List<Usuario> _usuariosNotificados;
+    private List<Usuario> _vistaPorUsuarios;
+
 
     public string Mensaje
     {
         get => _mensaje;
         set => _mensaje = value;
     }
-    public Notificacion(string mensaje)
-    {
-        Mensaje = mensaje;
-        UsuariosNotificados = new List<Usuario>();
-    }
-    
     public List<Usuario> UsuariosNotificados
     {
         get => _usuariosNotificados;
         set => _usuariosNotificados = value;
     }
+    
+    public List<Usuario> VistaPorUsuarios
+    {
+        get => _vistaPorUsuarios;
+        set => _vistaPorUsuarios=value;
+    }
+    public Notificacion(string mensaje)
+    {
+        Mensaje = mensaje;
+        UsuariosNotificados = new List<Usuario>();
+        VistaPorUsuarios = new List<Usuario>();
+    }
 
     public void AgregarUsuario(Usuario usuario)
     {
         UsuariosNotificados.Add(usuario);
+    }
+
+    public void MarcarComoVista(Usuario usuario)
+    {
+        VistaPorUsuarios.Add(usuario);
     }
 }
