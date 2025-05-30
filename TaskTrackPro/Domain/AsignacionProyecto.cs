8bbb7042b18de418c54d6671f1f381587f9c25f0
Mateo <mateomcelano@gmail.com>
2025-05-29 22:50:34 -0300

    refactor: cree clase AsginacionProyecto

---
+++ TaskTrackPro/Domain/AsignacionProyecto.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+
+namespace Domain;
+
+public class AsignacionProyecto
+{
+    private Proyecto _proyecto;
+    private Usuario _usuario;
+    private Rol _rol;
+    
+    public Proyecto Proyecto { get => _proyecto; set => _proyecto = value; }
+    public Usuario Usuario { get => _usuario; set => _usuario = value; }
+    public Rol Rol { get => _rol; set => _rol = value; }
+
+    public AsignacionProyecto(Proyecto proyecto, Usuario usuario, Rol rol)
+    {
+        Proyecto = proyecto;
+        Usuario = usuario;
+        Rol = rol;
+    }
+}
