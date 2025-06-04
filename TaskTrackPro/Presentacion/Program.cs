1a4045a34fa90c63bece869265878398336fd35a
paubrenlla <pau.brenlla@gmail.com>
2025-06-04 15:16:08 -0300

    fix: update de programcs para recurso

--- TaskTrackPro/Presentacion/Program.cs
+++ TaskTrackPro/Presentacion/Program.cs
@@ -2,9 +2,11 @@ using Blazored.LocalStorage;
 using Controllers;
 using DataAccess;
 using Domain;
+using Domain.Observers;
 using IDataAcces;
 using Presentacion.Components;
 using Services;
+using Services.Observers;
 using UserInterface.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,14 +20,22 @@ builder.Services.AddScoped<IDataAccessUsuario, UsuarioDataAccess>();
 builder.Services.AddScoped<IDataAccessProyecto, ProyectoDataAccess>();
 builder.Services.AddScoped<IDataAccessTarea, TareaDataAccess>();
 builder.Services.AddScoped<IDataAccessAsignacionProyecto, AsignacionProyectoDataAccess>();
+builder.Services.AddScoped<IDataAccessRecurso, RecursoDataAccess>();
+builder.Services.AddScoped<IDataAccessAsignacionRecursoTarea, AsignacionRecursoTareaDataAccess>();
+
+builder.Services.AddScoped<IRecursoObserver, ActualizadorEstadoTareas>();
 
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IProyectoService, ProyectoService>();
 builder.Services.AddScoped<ITareaService, TareaService>();
+builder.Services.AddScoped<IRecursoService, RecursoService>();
+builder.Services.AddScoped<IAsignacionRecursoTareaService, AsignacionRecursoTareaService>();
 
 builder.Services.AddScoped<UsuarioController>();
 builder.Services.AddScoped<ProyectoController>();
 builder.Services.AddScoped<TareaController>();
+builder.Services.AddScoped<RecursoController>();
+builder.Services.AddScoped<AsignacionRecursoTareaControllers>();
 
 builder.Services.AddScoped<SessionLogic>();
 
@@ -37,6 +47,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var servicio = scope.ServiceProvider;
+
+    var recursoService = servicio.GetRequiredService<IRecursoService>();
+    var observer = servicio.GetRequiredService<IRecursoObserver>();
+
+    recursoService.AgregarObservador(observer);
 }
 
 app.UseHttpsRedirection();
