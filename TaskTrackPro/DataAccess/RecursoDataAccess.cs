e3b694fbb02429d8eb02ac3893f890331e86e486
paubrenlla <pau.brenlla@gmail.com>
2025-06-05 16:39:44 -0300

    feat: datos precargados

--- TaskTrackPro/DataAccess/RecursoDataAccess.cs
+++ TaskTrackPro/DataAccess/RecursoDataAccess.cs
@@ -10,60 +10,6 @@ public class RecursoDataAccess :IDataAccessRecurso
     public RecursoDataAccess()
     {
         _listaRecursos = new List<Recurso>();
-
-        Recurso recurso1 = new Recurso(
-            "Auto Rojo",
-            "Vehiculo",
-            "Auto rojo electrico",
-            false,
-            1
-        );
-        _listaRecursos.Add(recurso1);
-        
-        Recurso recurso2 = new Recurso(
-            "Desarrollador Frontend",
-            "Humano",
-            "Experto en tecnologías como React, Angular y Vue.js",
-            true,
-            2
-        );
-        _listaRecursos.Add(recurso2);
-
-        Recurso recurso3 = new Recurso(
-            "Desarrollador Backend",
-            "Humano",
-            "Especialista en Node.js, Python y bases de datos SQL/NoSQL",
-            true,
-            3
-        );
-        _listaRecursos.Add(recurso3);
-
-        Recurso recurso4 = new Recurso(
-            "Desarrollador Full Stack",
-            "Humano",
-            "Con habilidades tanto en frontend como en backend",
-            true,
-            4
-        );
-        _listaRecursos.Add(recurso4);
-
-        Recurso recurso5 = new Recurso(
-            "Laptop de Desarrollo",
-            "Equipo Electrónico",
-            "Computadora portátil con alto rendimiento para programación",
-            false,
-            5
-        );
-        _listaRecursos.Add(recurso5);
-
-        Recurso recurso6 = new Recurso(
-            "Servidor Dedicado",
-            "Equipo Electrónico",
-            "Máquina potente para alojar aplicaciones y servicios web",
-            false,
-            6
-        );
-        _listaRecursos.Add(recurso6);
     }
     public void Add(Recurso recurso)
     {
