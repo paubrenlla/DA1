ffbea8a4dd2e3631cde4130403df9e2d9d32903d
fgavello <fgavello@ipxon.com>
2025-06-16 15:36:51 -0300

    delete exportaciones dataAccess

--- TaskTrackPro/IDataAccess/IDataAccessProyecto.cs
+++ TaskTrackPro/IDataAccess/IDataAccessProyecto.cs
@@ -5,6 +5,4 @@ namespace IDataAcces;
 public interface IDataAccessProyecto : IDataAccessGeneric<Proyecto>
 {
     public void Update(Proyecto proyecto);
-    string ExportarJSON();
-    string ExportarCSV();
 }
