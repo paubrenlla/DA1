43f28d0ea358050c1f3ba2e46d6bb47be2f69c3e
paubrenlla <pau.brenlla@gmail.com>
2025-06-17 02:31:45 -0300

    feat: inserts de db

---
+++ RecursosINSERT.sql
@@ -0,0 +1,9 @@
+INSERT INTO TaskTrackProDB.dbo.Recursos (Nombre,Tipo,Descripcion,SePuedeCompartir,CantidadDelRecurso,CantidadEnUso) VALUES
+	 (N'Recurso 1 - 1T',N'Recurso',N'Recurso',1,1,0),
+	 (N'Recurso 2 - 2T',N'Recurso',N'Recurso',1,2,0),
+	 (N'Recurso 3 - 5T',N'Recurso',N'Recurso',1,5,0),
+	 (N'Recurso 4 - 1F',N'Recurso',N'Recurso',0,1,0),
+	 (N'Recurso 5 - 2F',N'Recurso',N'Recurso',0,2,0),
+	 (N'Recurso 6 - 5F',N'Recurso',N'Recurso',0,5,0),
+	 (N'Recurso 7 - 10T',N'Recurso',N'Recurso',1,10,0),
+	 (N'Recurso 8 - 10F',N'Recurso',N'Recurso',0,10,0);
---
+++ UsuariosINSERT.sql
@@ -0,0 +1,11 @@
+INSERT INTO TaskTrackProDB.dbo.Usuarios (EsAdminSistema,Email,Nombre,Apellido,Pwd,PwdSerializado,FechaNacimiento) VALUES
+	 (1,N'admin@ejemplo.com',N'Ada',N'Lovelace',N'Q29udHJhc2XDsWExIQ==',N'Q29udHJhc2XDsWExIQ==','1985-12-10 00:00:00.0000000'),
+	 (0,N'usuario1@mail.com',N'Mateo',N'Muniz',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','2000-01-01 00:00:00.0000000'),
+	 (0,N'usuario2@mail.com',N'Paula',N'Brenlla',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','2000-01-01 00:00:00.0000000'),
+	 (0,N'usuario3@mail.com',N'Fernando',N'Gavello',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','2000-01-01 00:00:00.0000000'),
+	 (0,N'usuario4@mail.com',N'Luis',N'Simon',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','2000-01-01 00:00:00.0000000'),
+	 (0,N'usuario5@mail.com',N'Santiago',N'Perez',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','2000-01-01 00:00:00.0000000'),
+	 (0,N'usuario6@mail.com',N'Juan',N'Diaz',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','2000-01-01 00:00:00.0000000'),
+	 (0,N'usuario7@mail.com',N'Harry',N'Potter',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','1980-07-31 00:00:00.0000000'),
+	 (0,N'usuario8@mail.com',N'George R R',N'Martin',N'Q29udHJhc2VuYTEh',N'Q29udHJhc2VuYTEh','1948-09-20 00:00:00.0000000'),
+	 (0,N'usuario9@mail.com',N'J R R',N'Tolkien',N'Q29udHJhc2XDsWExIQ==',N'Q29udHJhc2XDsWExIQ==','1892-01-03 00:00:00.0000000');
