e1afc616ccf1a6f927d003d58081e235359f3452
paubrenlla <pau.brenlla@gmail.com>
2025-06-17 01:47:10 -0300

    refactor: cleancode i dataaccess generic

--- TaskTrackPro/IDataAccess/IDataAccessGeneric.cs
+++ TaskTrackPro/IDataAccess/IDataAccessGeneric.cs
@@ -8,7 +8,5 @@ public interface IDataAccessGeneric <T>
 
     public T? GetById(int Id);
     
-   // public void Update(T data); TODO cuando termine con integracion a DataBase
-
     public List<T> GetAll();
 }
